dc7fa4543749c309b196ca65ae7d630816dfc723
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-12-13 15:19:56 -0600

    Other

--- orrblog/orrblog/urls.py
+++ orrblog/orrblog/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('markdownx/', include('markdownx.urls')),
-]
-
 from django.conf import settings
 from django.conf.urls.static import static
+
+# Put admin at the bottom so it doesn't get overriden
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('markdownx/', include('markdownx.urls')),
+    path('admin/', admin.site.urls),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
