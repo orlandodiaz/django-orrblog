321f8cd305a582a1b597322ee6e17748cb0c7e92
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-01 17:11:43 -0600

    No commit

--- orrblog/settings.py
+++ orrblog/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'orrblog.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 # BELOW IS FOR LOCAL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
