37db7a3a7507dfba22fb93e2bcf86c2c21d7ff2d
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-01 17:27:02 -0600

    Rearrange files

--- orrblog/settings.py
+++ orrblog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_ROOT = "static/"
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
