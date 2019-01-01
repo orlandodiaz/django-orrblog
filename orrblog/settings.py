47fff49addda8152d71d33283a0205d7f8503cb7
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-01 17:10:18 -0600

    No commit message

--- orrblog/settings.py
+++ orrblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +25,7 @@ SECRET_KEY = '5!r+f6l6lrtmh7ns)yokxjbcao%$z1n&xzy6gkv&@5rp-sb7zq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '0.0.0.0', 'localhost', '127.0.0.1', 'shielded-earth-89095.herokuapp.com'
-]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'orrblog.herokuapp.com']
 
 # Application definition
 
@@ -75,12 +74,16 @@ WSGI_APPLICATION = 'orrblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# BELOW IS FOR LOCAL
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
