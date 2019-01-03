3f6535aa601bcc5ace39872410392fa13bee894a
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 14:57:39 -0600

    Fix typo

--- orrblog/settings/base.py
+++ orrblog/settings/base.py
@@ -25,10 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '0.0.0.0', 'localhost', '127.0.0.1', 'orrblog.herokuapp.com',
-    'orlandodiaz.co'
-]
+# ALLOWED_HOSTS = [
+#     '0.0.0.0', 'localhost', '127.0.0.1', 'orrblog.herokuapp.com',
+#     'orlandodiaz.co'
+# ]
 
 # Application definition
 
