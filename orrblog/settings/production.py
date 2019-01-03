f38e7d211e8ab9e6f885622f2bb737e4fd559c16
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 02:50:37 -0600

    Add secret key to production settings

--- orrblog/settings/production.py
+++ orrblog/settings/production.py
@@ -1,6 +1,12 @@
 from .base import *
+from boto.s3.connection import S3Connection
 
 DEBUG = True
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECRET_KEY = S3Connection(os.environ['SECRET_KEY'], os.environ['SECRET_KEY'])
+# print(SECRET_KEY)
