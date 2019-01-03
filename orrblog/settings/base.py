e6eea6150cf2eed6de998aa74dc047692ff11659
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 03:41:01 -0600

    Change STATIC_ROOT behavior

--- orrblog/settings/base.py
+++ orrblog/settings/base.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = "static/"
+# STATIC_ROOT = "static/"
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 #
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.spath.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 #
 # # Extra places for collectstatic to find static files.
