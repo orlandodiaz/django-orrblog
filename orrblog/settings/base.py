d8776d90192974710b28bc6fc8adabc4aeb66c35
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 03:37:44 -0600

    Clean up

--- orrblog/settings/base.py
+++ orrblog/settings/base.py
@@ -80,8 +80,6 @@ WSGI_APPLICATION = 'orrblog.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(
-#     conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
