7fa51d08ea5a77e9e6872def96966b8c93a6af48
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 14:56:38 -0600

    Move ALLOWED_HOSTS to production. Change default settings to development settings

--- orrblog/settings/production.py
+++ orrblog/settings/production.py
@@ -2,6 +2,11 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    '0.0.0.0', 'localhost', '127.0.0.1', 'orrblog.herokuapp.com',
+    'orlandodiaz.co'
+]
+
 # Use Heroku Postgres database from the DATABASE_URL environmental config variable
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
