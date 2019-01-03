f8de8730cdb78272a74bed3522ca48122c7c14e4
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-03 03:12:09 -0600

    Clean up

--- orrblog/settings/production.py
+++ orrblog/settings/production.py
@@ -4,6 +4,3 @@ DEBUG = True
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
-
-# SECRET_KEY = S3Connection(os.environ['SECRET_KEY'], os.environ['SECRET_KEY'])
-# print(SECRET_KEY)
